9fafa19cff93c46f551375879061b1772f4f4a84
Julian Holfeld <julian@uni-kassel.de>
2022-01-07 13:03:05 +0100

    feat: filter item-size

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import requests
 from xml.etree.ElementTree import fromstring
 
 
-def fetch(url: str, page: int):
+def fetch(url: str, page: int) -> bool:
     content = requests.get(url, params={'page': page}).content
     xml = fromstring(content)
 
+    if not len(xml.findall('channel/item')):
+        return False
+
+    return True
+
 
 if __name__ == '__main__':
-    fetch('https://itch.io/games/made-with-godot.xml', 1)
+    print(fetch('https://itch.io/games/made-with-godot.xml', 10000))
